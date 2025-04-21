82ec6727a941cf444ea505722fe54720f6c743eb
kaitlinchoffmann  <kaitlinchoffmann@gmail.com>
2025-04-21 14:38:34 -0400

    Add updateUser and deleteAccount

--- server/models/user.js
+++ server/models/user.js
@@ -31,12 +31,7 @@ async function getAllUsers() {
   let sql = `SELECT * FROM User`
   return await con.query(sql)
 }
-// USER Example:
-const user = {
-  Username: "Bobbyiscool",
-  Email: "b@b",
-  Password: "cathysucks"
-}
+
 // READ in CRUD: Logging in a user
 async function login(user) {
   let cUser = await userExists(user.Username)
@@ -49,11 +44,51 @@ async function login(user) {
 async function userExists(username) {
   let sql = `
     SELECT * FROM User
-    WHERE Username="${username}"
+    WHERE username="${username}"
   `
   return await con.query(sql)
 }
 
 // CREATE in CRUD - Registering a user
+async function register(user) {
+  const cUser = await userExists(user.Username)
+  if(cUser.length > 0) throw Error("Username already in use!")
 
-module.exports = { getAllUsers, login }
+  let sql = `
+    INSERT INTO User(password, username, email, firstName, lastName)
+    VALUES("${user.Password}", "${user.Username}", "${user.Email}", "${user.FirstName}", "${user.LastName}")
+  `
+  await con.query(sql)
+
+  return await login(user)
+}
+
+
+async function editUsername(user) {
+  let sql = `
+    UPDATE User SET
+    username = "${user.username}"
+    WHERE userId = ${user.userId}
+  `
+  await con.query(sql)
+  const currentUser = await userExists(user.username)
+  return currentUser[0]
+}
+
+// USER Example:
+const user = {
+  Username: "Bobbyiscool",
+  Email: "b@b",
+  Password: "cathysucks",
+  FirstName: "Bob",
+  LastName: "Bobby"
+}
+async function deleteAccount(user) {
+  let sql = `
+    DELETE FROM user
+    WHERE userId = ${user.userId}
+  `
+  await con.query(sql)
+}
+
+module.exports = { getAllUsers, login, register, editUsername, deleteAccount }
--- server/routes/user.js
+++ server/routes/user.js
@@ -3,7 +3,6 @@ const User = require("../models/user")
 const router = express.Router()
 
 router
-
 .get('/getUsers', async (req, res) => {
   try {
     const users = await User.getAllUsers()
@@ -17,6 +16,33 @@ router
   try {
     const user = await User.login(req.body)
     res.send({...user, password: undefined})
+  } catch(err) {
+    res.status(401).send({message: err.message})
+  }
+})
+
+.post('/register', async (req, res) => {
+  try {
+    const user = await User.register(req.body)
+    res.send({...user, password: undefined})
+  } catch(err) {
+    res.status(401).send({message: err.message})
+  }
+})
+
+.put('/update', async (req, res) => {
+  try {
+    const user = await User.editUsername(req.body)
+    res.send({...user, password: undefined})
+  } catch(err) {
+    res.status(401).send({message: err.message})
+  }
+})
+
+.delete('/deleteAccount', async (req, res) => {
+  try {
+    await User.deleteAccount(req.body)
+    res.send({success: "HOW DARE YOU!!! I did everything for you!!!!! You will come to regret this...this is a THREAT!!! >:(((("})
   } catch(err) {
     res.status(401).send({message: err.message})
   }
