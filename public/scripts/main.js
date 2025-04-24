c37af5adc73ca0487955f497a7f802969c0e22b6
kaitlinchoffmann  <kaitlinchoffmann@gmail.com>
2025-04-24 10:47:04 -0400

    Reformat code

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,3 +1,18 @@
+// Fetch method implementation:
+export async function fetchData(route = '', data = {}, methodType) {
+  const response = await fetch(`http://localhost:3000${route}`, {
+    method: methodType, // *POST, PUT, DELETE, etc.
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data) // body data type must match "Content-Type" header
+  });
+  if (response.ok) {
+    return await response.json(); // parses JSON response into native JavaScript objects
+  } else {
+    throw await response.json();
+  }
+}
 // window.alert("Welcome to Class!!!! :)")
 let loginForm = document.getElementById('loginForm')
 loginForm.addEventListener('submit', login)
